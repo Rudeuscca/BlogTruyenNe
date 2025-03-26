671bc806553e643cdd79723636cd42eeb6563e37
Rudeuscca <thienthaothienan@gmail.com>
2025-03-26 23:57:49 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Trang web đã sẵn sàng!");
+    // Bạn có thể thêm code JS để xử lý các sự kiện, hiệu ứng, v.v...
+  });
+  
